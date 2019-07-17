56da25a23dd93afb690a757110d944f13df5035b
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-17 23:44:39 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -11,7 +11,7 @@ class Game
     [2, 4, 6]
   ]
 
-  def initialize(player_1 = Players::Human.new("X"), player_2= Players::Human.new("O"), board)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
 
   end
 
