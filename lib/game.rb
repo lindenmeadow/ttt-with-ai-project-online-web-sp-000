d6bee15a4917251d2923c4bc0ce52c9dd566e375
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-18 00:35:08 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -27,7 +27,9 @@ class Game
 
   def won?
     WIN_COMBINATIONS.detect do |winner|
-      @board.cells[winner[0]] == @board.cells[winner[1]] && @board.cells[winner[1]] == @board.cells[winner[2]]
+      @board.cells[winner[0]] == @board.cells[winner[1]] &&
+      @board.cells[winner[1]] == @board.cells[winner[2]] &&
+      @board.cells[winner[0]] ==
     end
   end
 
